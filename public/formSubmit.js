44b0d61ee6ac8641056fe03a466f658497c1631d
CRSAds <yuri@crsadvertising.com>
2025-10-07 19:37:21 +0200

    Update formSubmit.js

--- public/formSubmit.js
+++ public/formSubmit.js
@@ -87,7 +87,7 @@ async function fetchLead(payload) {
   }
 
   try {
-    const response = await fetch("/api/lead.js", {
+    const response = await fetch("/api/lead", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
